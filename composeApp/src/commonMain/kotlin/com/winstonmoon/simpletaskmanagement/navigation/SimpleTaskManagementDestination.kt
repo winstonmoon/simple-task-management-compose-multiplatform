adfce895ba0545bf019db163d82a720df22f76b5
Sungjun Moon <project_m_@hotmail.com>
2024-10-10 23:25:39 +0900

    delete SimpleTaskManagementDestination

--- composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/navigation/SimpleTaskManagementDestination.kt
+++
@@ -1,8 +0,0 @@
-package com.winstonmoon.simpletaskmanagement.navigation
-
-sealed class SimpleTaskManagementDestination(val route: String) {
-    data object AchievementScreen : SimpleTaskManagementDestination("achievement")
-    data object InputTaskScreen : SimpleTaskManagementDestination("input_task")
-    data object SettingsScreen : SimpleTaskManagementDestination("settings")
-    data object TaskScreen : SimpleTaskManagementDestination("task")
-}
