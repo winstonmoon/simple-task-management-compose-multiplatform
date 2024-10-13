36a70379071904604b480db40656ac52874cfc2d
Sungjun Moon <project_m_@hotmail.com>
2024-10-13 23:47:34 +0900

    implement first screen in progress

--- composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/ui/screen/TaskScreen.kt
+++ composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/ui/screen/TaskScreen.kt
@@ -1,25 +1,42 @@
 package com.winstonmoon.simpletaskmanagement.ui.screen
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.winstonmoon.simpletaskmanagement.ui.theme.SimpleTaskManagementTheme
 import kotlinx.serialization.Serializable
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Serializable
 data object TaskRoute
 
 @Composable
 fun TaskRoute(
-
+    modifier: Modifier = Modifier,
 ) {
-    TaskScreen()
+    TaskScreen(
+        modifier = modifier,
+    )
 }
 
 @Composable
 internal fun TaskScreen(
-
+    modifier: Modifier = Modifier,
 ) {
-    Box {
+    Box(
+        modifier = modifier
+            .fillMaxSize(),
+    ) {
         Text("hello world")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewTaskScreen() {
+    SimpleTaskManagementTheme {
+        TaskScreen()
     }
 }
