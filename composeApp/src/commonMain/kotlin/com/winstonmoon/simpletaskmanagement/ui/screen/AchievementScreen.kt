0b312e45b960ae28c6077476f9757605f291d01b
Sungjun Moon <project_m_@hotmail.com>
2024-10-20 23:51:27 +0900

    implement AchievementScreen in progress

--- composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/ui/screen/AchievementScreen.kt
+++ composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/ui/screen/AchievementScreen.kt
@@ -1,7 +1,10 @@
 package com.winstonmoon.simpletaskmanagement.ui.screen
 
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.winstonmoon.simpletaskmanagement.ui.component.CustomAppBar
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -9,16 +12,28 @@ data object AchievementRoute
 
 @Composable
 fun AchievementRoute(
+    drawerState: DrawerState,
     modifier: Modifier = Modifier,
 ) {
     AchievementScreen(
+        drawerState = drawerState,
         modifier = modifier,
     )
 }
 
 @Composable
 internal fun AchievementScreen(
+    drawerState: DrawerState,
     modifier: Modifier = Modifier,
 ) {
+    Scaffold(
+        topBar = {
+            CustomAppBar(
+                drawerState = drawerState,
+                title = "Achievement"
+            )
+        }
+    ) { paddingValues ->
 
+    }
 }
