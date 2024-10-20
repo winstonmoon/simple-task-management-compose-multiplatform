529d94f6b80400e446f012991f8d9dd4ff422a24
Sungjun Moon <project_m_@hotmail.com>
2024-10-20 23:51:11 +0900

    implement CustomAppBar

---
+++ composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/ui/component/CustomAppBar.kt
@@ -0,0 +1,44 @@
+package com.winstonmoon.simpletaskmanagement.ui.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomAppBar(
+    drawerState: DrawerState?,
+    title: String,
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    CenterAlignedTopAppBar(
+        navigationIcon = {
+            if (drawerState != null) {
+                IconButton(
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.open()
+                        }
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Menu,
+                        contentDescription = null,
+                    )
+                }
+            }
+        },
+        title = {
+            Text(text = title)
+        }
+    )
+}
