2153a455557bd947d4c55263522666446dc1ba8a
Sungjun Moon <project_m_@hotmail.com>
2024-10-20 23:51:38 +0900

    implement TaskScreen in progress

--- composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/ui/screen/TaskScreen.kt
+++ composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/ui/screen/TaskScreen.kt
@@ -1,42 +1,65 @@
 package com.winstonmoon.simpletaskmanagement.ui.screen
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.winstonmoon.simpletaskmanagement.ui.theme.SimpleTaskManagementTheme
+import androidx.compose.ui.unit.dp
+import com.winstonmoon.simpletaskmanagement.ui.component.CustomAppBar
 import kotlinx.serialization.Serializable
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Serializable
 data object TaskRoute
 
 @Composable
 fun TaskRoute(
+    drawerState: DrawerState,
     modifier: Modifier = Modifier,
 ) {
     TaskScreen(
+        drawerState= drawerState,
         modifier = modifier,
     )
 }
 
 @Composable
 internal fun TaskScreen(
+    drawerState: DrawerState,
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = modifier
-            .fillMaxSize(),
-    ) {
-        Text("hello world")
-    }
-}
-
-@Preview
-@Composable
-fun PreviewTaskScreen() {
-    SimpleTaskManagementTheme {
-        TaskScreen()
+    Scaffold(
+        topBar = {
+            CustomAppBar(
+                drawerState = drawerState,
+                title = "Task"
+            )
+        }
+    ) { paddingValues ->
+        BoxWithConstraints(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) {
+            FloatingActionButton(
+                onClick = {
+                },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(12.dp),
+                ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = null,
+                )
+            }
+        }
     }
 }
