7e8dd074313bd0e7bd1cb5ad57a696b725111430
Sungjun Moon <project_m_@hotmail.com>
2024-10-27 23:41:53 +0900

    delete unused code

--- composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.winstonmoon.simpletaskmanagement
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.winstonmoon.simpletaskmanagement
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
