a71987eb2959e27e9844d522c81581d862f3fd33
Sungjun Moon <project_m_@hotmail.com>
2024-11-15 23:42:55 +0900

    change fab color

--- composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/ui/component/CustomFloatingActionButton.kt
+++ composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/ui/component/CustomFloatingActionButton.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -31,5 +32,6 @@ fun CustomFloatingActionButton(
             )
         },
         onClick = onClick,
+        containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
     )
 }
