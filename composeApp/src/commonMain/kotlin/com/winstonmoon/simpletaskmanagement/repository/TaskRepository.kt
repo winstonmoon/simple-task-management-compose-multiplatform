93377a4b4b6688930752bc3dc720cc7dc23ba37a
Sungjun Moon <project_m_@hotmail.com>
2025-03-27 23:26:35 +0900

    implement taskrepository

---
+++ composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/repository/TaskRepository.kt
@@ -0,0 +1,45 @@
+package com.winstonmoon.simpletaskmanagement.repository
+
+import com.winstonmoon.simpletaskmanagement.cache.Task
+import com.winstonmoon.simpletaskmanagement.data.local.DatabaseHelper
+import kotlinx.coroutines.flow.Flow
+
+class TaskRepository(
+    private val dbHelper: DatabaseHelper,
+) {
+    suspend fun insertTask(
+        title: String,
+        status: String,
+        priority: String,
+        dueDate: Long?,
+    ) {
+        dbHelper.insertTask(
+            title = title,
+            status = status,
+            priority = priority,
+            dueDate = dueDate,
+        )
+    }
+
+    suspend fun updateTask(
+        id:Long,
+        title: String,
+        status: String,
+        priority: String,
+        dueDate: Long?,
+    ) {
+        dbHelper.updateTask(
+            id = id,
+            title = title,
+            status = status,
+            priority = priority,
+            dueDate = dueDate,
+        )
+    }
+
+    suspend fun deleteTask(id: Long) {
+        dbHelper.deleteTask(id = id)
+    }
+
+    fun getTasks(): Flow<List<Task>> = dbHelper.selectAllTasks()
+}
