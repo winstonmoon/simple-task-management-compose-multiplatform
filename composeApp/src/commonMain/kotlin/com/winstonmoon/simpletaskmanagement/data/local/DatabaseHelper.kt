47052880e1eb7dbe7a419930062db0d1779f7e52
Sungjun Moon <project_m_@hotmail.com>
2025-03-27 23:26:25 +0900

    implement databasehelper

--- composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/data/local/DatabaseHelper.kt
+++ composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/data/local/DatabaseHelper.kt
@@ -1,10 +1,66 @@
 package com.winstonmoon.simpletaskmanagement.data.local
 
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
 import app.cash.sqldelight.db.SqlDriver
+import com.winstonmoon.simpletaskmanagement.cache.SimpleTaskManagementDatabase
+import com.winstonmoon.simpletaskmanagement.cache.composeApp.newInstance
+import com.winstonmoon.simpletaskmanagement.cache.Task
+import com.winstonmoon.simpletaskmanagement.sqldelight.transactionWithContext
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 
 class DatabaseHelper(
     sqlDriver: SqlDriver,
     private val backgroundDispatcher: CoroutineDispatcher
 ) {
+    private val dbRef: SimpleTaskManagementDatabase = SimpleTaskManagementDatabase::class.newInstance(sqlDriver)
+
+    suspend fun insertTask(
+        title: String,
+        status: String,
+        priority: String,
+        dueDate: Long?,
+    ) {
+        dbRef.transactionWithContext(backgroundDispatcher) {
+            dbRef.simpleTaskManagementDatabaseQueries.insertTask(
+                title = title,
+                status = status,
+                priority = priority,
+                dueDate = dueDate,
+            )
+        }
+    }
+
+    suspend fun updateTask(
+        id:Long,
+        title: String,
+        status: String,
+        priority: String,
+        dueDate: Long?,
+    ) {
+        dbRef.transactionWithContext(backgroundDispatcher) {
+            dbRef.simpleTaskManagementDatabaseQueries.updateTask(
+                id = id,
+                title = title,
+                status = status,
+                priority = priority,
+                dueDate = dueDate
+            )
+        }
+    }
+
+    suspend fun deleteTask(id: Long) {
+        dbRef.transactionWithContext(backgroundDispatcher) {
+            dbRef.simpleTaskManagementDatabaseQueries.deleteTask(id = id)
+        }
+    }
+
+    fun selectAllTasks(): Flow<List<Task>> = dbRef.simpleTaskManagementDatabaseQueries
+        .selectAllTasks()
+        .asFlow()
+        .mapToList(Dispatchers.Default)
+        .flowOn(backgroundDispatcher)
 }
