2f341975b6be6c915cea9298d1b83a3116843548
Sungjun Moon <project_m_@hotmail.com>
2025-04-30 23:55:49 +0900

    change stateflow

--- composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/ui/screen/TaskViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/ui/screen/TaskViewModel.kt
@@ -4,14 +4,27 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.winstonmoon.simpletaskmanagement.model.Task
 import com.winstonmoon.simpletaskmanagement.repository.TaskRepository
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class TaskViewModel(
     private val taskRepository: TaskRepository,
 ) : ViewModel() {
+
+    private val _readyTask = MutableStateFlow<List<Task>>(emptyList())
+    val readyTask: StateFlow<List<Task>> = _readyTask
+
+    init {
+        viewModelScope.launch {
+            taskRepository.getTasksByStatus(status = "READY").collectLatest {
+                _readyTask.value = it
+            }
+        }
+    }
 
     val readyTasks: StateFlow<List<Task>> = taskRepository.getTasksByStatus(status = "READY")
         .stateIn(
