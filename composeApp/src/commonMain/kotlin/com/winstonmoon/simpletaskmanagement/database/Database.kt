d2d7a86e2e3821fe3d05bb55ec4a4fc8cf724501
Sungjun Moon <project_m_@hotmail.com>
2025-05-06 23:56:49 +0900

    resolve build error

--- composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/database/Database.kt
+++ composeApp/src/commonMain/kotlin/com/winstonmoon/simpletaskmanagement/database/Database.kt
@@ -1,13 +1,16 @@
 package com.winstonmoon.simpletaskmanagement.database
 
-import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.RoomDatabaseConstructor
 import com.winstonmoon.simpletaskmanagement.database.dao.TaskDao
 import com.winstonmoon.simpletaskmanagement.database.model.TaskEntity
 
 @Database(entities = [TaskEntity::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
+abstract class AppDatabase : RoomDatabase(), DB {
     abstract fun taskDao(): TaskDao
+    override fun clearAllTables(): Unit {}
+}
+
+interface DB {
+    fun clearAllTables(): Unit {}
 }
