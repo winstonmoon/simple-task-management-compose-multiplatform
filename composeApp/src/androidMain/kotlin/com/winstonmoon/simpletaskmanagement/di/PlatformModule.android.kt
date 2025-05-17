0108157b5ea3d903cc94ec12d6f2c5676c9128af
Sungjun Moon <project_m_@hotmail.com>
2025-05-17 23:53:06 +0900

    change koin

--- composeApp/src/androidMain/kotlin/com/winstonmoon/simpletaskmanagement/di/PlatformModule.android.kt
+++ composeApp/src/androidMain/kotlin/com/winstonmoon/simpletaskmanagement/di/PlatformModule.android.kt
@@ -1,20 +1,9 @@
 package com.winstonmoon.simpletaskmanagement.di
 
-import androidx.room.RoomDatabase
 import com.winstonmoon.simpletaskmanagement.database.AppDatabase
 import com.winstonmoon.simpletaskmanagement.database.getDatabaseBuilder
 import org.koin.core.module.Module
 import org.koin.dsl.module
-
-//actual fun platformModule(context: Context): Module = module {
-//    single<SqlDriver> {
-//        AndroidSqliteDriver(
-//            SimpleTaskManagementDatabase.Schema,
-//            context = context,
-//            "SimpleTaskManagementDatabase"
-//        )
-//    }
-//}
 
 actual fun platformModule(): Module = module {
     single<AppDatabase> { getDatabaseBuilder(get()) }
